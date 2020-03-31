5cd08b572b73ca05268446b9507f0f9cd6aae8d0
Robert San <robertsandru96@gmail.com>
2020-03-31 20:14:49 +0300

    Firebase integration started

---
+++ src/constants/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBLodHhyW_BI6hmboFbULOKNddu2UiNNxA",
+  authDomain: "developerscheatsheet.firebaseapp.com",
+  databaseURL: "https://developerscheatsheet.firebaseio.com",
+  projectId: "developerscheatsheet",
+  storageBucket: "developerscheatsheet.appspot.com",
+  messagingSenderId: "877751460278",
+  appId: "1:877751460278:web:40b650f5904035c6836117",
+  measurementId: "G-6XW2E8LBRR"
+};
+
+export default firebaseConfig;
